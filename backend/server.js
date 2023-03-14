1efceef9863ecb563bef7f66d1e24d1d0d2410be
xlwillia <67807020+xlwillia@users.noreply.github.com>
2023-03-14 18:15:40 -0500

    [backend] Created and Tested CRUD for first tables Created and tested CRUD for Employee, Customer, Vendor. Update still needs work.

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,7 @@ async function createOP(query, binds){
     }
 };
 // READ
-async function readOP(query){
+async function readOP(query, binds){
     let connection;
     try{
         //CONNECTION OPEN
@@ -72,7 +72,7 @@ async function readOP(query){
         console.log("Successfully connected to database");
 
         // EXECUTION
-        result = await connection.execute(query);
+        result = await connection.execute(query,binds);
         return result
     }catch(err){
         console.log(err);
@@ -152,55 +152,215 @@ async function deleteOP(query, binds){
 /* EMPLOYEE */
 // CREATE
 app.post('/employee', function(req, res){
-  const empAttributes = ':emp_Id,:emp_address,:city,:state,:zip,:phone,:datehired,:Lname,:Fname,:sex'
-  let emp_Id = '';
-  let emp_address = '';
-  let city = '';
-  let state = '';
-  let zip = '';
-  let phone = '';
-  let datehired = '';
-  let Lname = '';
-  let Fname = '';
-  let sex = '';
-  // Query Creation
-  let createQuery = 'INSERT INTO EMPLOYEE VALUES '+'('+empAttributes+')';
-  let createBinds = [[emp_Id,emp_address,city,state,zip,phone,datehired,Lname,Fname,sex]];
-  createOP(createQuery, createBinds);
+    // Column Names
+    const empAttributes = ':emp_id,:emp_address,:city,:state,:zip,:phone,:datehired,:Lname,:Fname,:sex';
+    // Values
+    let emp_Id = '';
+    let emp_address = '';
+    let city = '';
+    let state = '';
+    let zip = '';
+    let phone = '';
+    let datehired = '';
+    let Lname = '';
+    let Fname = '';
+    let sex = '';
+    // Query Creation
+    let createQuery = 'INSERT INTO EMPLOYEE VALUES '+'('+empAttributes+')';
+    let createBinds = [[emp_Id,emp_address,city,state,zip,phone,datehired,Lname,Fname,sex]];
+    createOP(createQuery, createBinds);
 });
 // READ
-app.get('/employee', function(req, res){
-  // Query Creation
-  let readQuery = 'SELECT * FROM EMPLOYEE';
-  readOP(readQuery);
+app.get('/employee', async function(req, res){
+    // Values
+    let restriction = ''; // Search by table attribute
+    let restrictionValue = ''; // Value of attribute
+
+    let isRestricted = false; // (TRUE = Where IS needed)/(false = WHERE IS NOT needed)
+    // Query Creation
+    if (isRestricted){
+        let readQuery = 'SELECT * FROM EMPLOYEE WHERE '+restriction+'=:restrictionValue';
+        let readBinds = [restrictionValue];
+        console.log(await readOP(readQuery,readBinds));
+    }else{
+        let readQuery = 'SELECT * FROM EMPLOYEE';
+        console.log(await readOP(readQuery));
+    }
 });
 // UPDATE
 app.put('/employee', function(req, res){
-  // Query Creation
-  let updateQuery = ''
-  let updateBinds = [];
-  updateOP(updateQuery, updateBinds);
+    // Values
+    let emp_id = '';
+    let emp_address = '';
+    let city = '';
+    let state = '';
+    let zip = '';
+    let phone = '';
+    let datehired = '';
+    let Lname = '';
+    let Fname = '';
+    let sex = '';
+    // Need a way to only update the columns that have been changed
+    let columnNames = '';
+    let updatedValues = '';
+    // Query Creation
+    let updateQuery = 'UPDATE EMPLOYEE SET '+columnNames+' = :updatedValues WHERE emp_id =:emp_id';
+    let updateBinds = [updatedValues, emp_id];
+    updateOP(updateQuery, updateBinds);
 });
 // DELETE
 app.delete('/employee', function(req, res){
-  let emp_Id = '';
-  // Query Creation
-  let deleteQuery = 'DELETE FROM EMPLOYEE WHERE id = :1';
-  let deleteBinds = [emp_Id];
-  deleteOP(deleteQuery, deleteBinds);
+    // Values
+    let emp_Id = '';
+    // Query Creation
+    let deleteQuery = 'DELETE FROM EMPLOYEE WHERE emp_id = :emp_id';
+    let deleteBinds = [emp_Id];
+    deleteOP(deleteQuery, deleteBinds);
 });
 
 /* CUSTOMER */
 // CREATE
+app.post('/customer', function(req, res){
+    // Column Names
+    const custAttributes = ':cust_id,:name,:address,:city,:state,:zip,:phone,:Edate,:company,:taxnum';
+    // Values
+    let cust_id = '';
+    let name = '';
+    let address = '';
+    let city = '';
+    let state = '';
+    let zip = '';
+    let phone = '';
+    let Edate = '';
+    let company = '';
+    let taxnum = '';
+    // Query Creation
+    let createQuery = 'INSERT INTO CUSTOMER VALUES '+'('+custAttributes+')';
+    let createBinds = [[cust_id,name,address,city,state,zip,phone,Edate,company,taxnum]];
+    createOP(createQuery, createBinds);
+});
 // READ
+app.get('/customer', async function(req, res){
+    // Values
+    let restriction = ''; // Search by table attribute
+    let restrictionValue = ''; // Value of attribute
+
+    let isRestricted = false; // (TRUE = Where IS needed)/(false = WHERE IS NOT needed)
+    // Query Creation
+    if (isRestricted){
+        let readQuery = 'SELECT * FROM CUSTOMER WHERE '+restriction+'=:restrictionValue';
+        let readBinds = [restrictionValue];
+        console.log(await readOP(readQuery,readBinds));
+    }else{
+        let readQuery = 'SELECT * FROM CUSTOMER';
+        console.log(await readOP(readQuery));
+    }
+});
 // UPDATE
+app.put('/customer', function(req, res){
+    // Values
+    let cust_id = '';
+    let name = '';
+    let address = '';
+    let city = '';
+    let state = '';
+    let zip = '';
+    let phone = '';
+    let Edate = '';
+    let company = '';
+    let taxnum = '';
+    // Need a way to only update the columns that have been changed
+    let columnNames = '';
+    let updatedValues = '';
+    // Query Creation
+    let updateQuery = 'UPDATE CUSTOMER SET '+columnNames+' = :updatedValues WHERE cust_id =:cust_id';
+    let updateBinds = [updatedValues, cust_id];
+    updateOP(updateQuery, updateBinds);
+});
 // DELETE
+app.delete('/customer', function(req, res){
+    // Values
+    let cust_id = '';
+    // Query Creation
+    let deleteQuery = 'DELETE FROM CUSTOMER WHERE cust_id = :cust_id';
+    let deleteBinds = [cust_id];
+    deleteOP(deleteQuery, deleteBinds);
+});
 
 /* VENDOR */
 // CREATE
+app.post('/vendor', function(req, res){
+    // Column Names
+    const vendAttributes = ':vendor_id,:ven_name,:address,:city,:state,:zip,:phone,:phone2,:fax,:contact,:Edate,:email,:keymap';
+    // Values
+    let vendor_id = '';
+    let ven_name = '';
+    let address = '';
+    let city = '';
+    let state = '';
+    let zip = '';
+    let phone = '';
+    let phone2 = '';
+    let fax = '';
+    let contact = '';
+    let Edate = '';
+    let email = '';
+    let keymap = '';
+    // Query Creation
+    let createQuery = 'INSERT INTO VENDOR VALUES '+'('+vendAttributes+')';
+    let createBinds = [[vendor_id,ven_name,address,city,state,zip,phone,phone2,fax,contact,Edate,email,keymap]];
+    createOP(createQuery, createBinds);
+});
 // READ
+app.get('/vendor', async function(req, res){
+    // Values
+    let restriction = ''; // Search by table attribute
+    let restrictionValue = ''; // Value of attribute
+
+    let isRestricted = false; // (TRUE = Where IS needed)/(false = WHERE IS NOT needed)
+    // Query Creation
+    if (isRestricted){
+        let readQuery = 'SELECT * FROM VENDOR WHERE '+restriction+'=:restrictionValue';
+        let readBinds = [restrictionValue];
+        console.log(await readOP(readQuery,readBinds));
+    }else{
+        let readQuery = 'SELECT * FROM CUSTOMER';
+        console.log(await readOP(readQuery));
+    }
+});
 // UPDATE
+app.put('/vendor', function(req, res){
+    // Values
+    let vendor_id = '';
+    let ven_name = '';
+    let address = '';
+    let city = '';
+    let state = '';
+    let zip = '';
+    let phone = '';
+    let phone2 = '';
+    let fax = '';
+    let contact = '';
+    let Edate = '';
+    let email = '';
+    let keymap = '';
+    // Need a way to only update the columns that have been changed
+    let columnNames = '';
+    let updatedValues = '';
+    // Query Creation
+    let updateQuery = 'UPDATE CUSTOMER SET '+columnNames+' = :updatedValues WHERE cust_id =:cust_id';
+    let updateBinds = [updatedValues, cust_id];
+    updateOP(updateQuery, updateBinds);
+});
 // DELETE
+app.delete('/vendor', function(req, res){
+    // Values
+    let vendor_id = '';
+    // Query Creation
+    let deleteQuery = 'DELETE FROM CUSTOMER WHERE vendor_id = :vendor_id';
+    let deleteBinds = [vendor_id];
+    deleteOP(deleteQuery, deleteBinds);
+});
 
 app.listen(PORT, () => {
     console.log(PORT, "is the magic port");
