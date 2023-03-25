beb6771f5dd0659436c05edd60c50d0f4393c2d7
dhuynh9 <89826984+dhuynh9@users.noreply.github.com>
2023-03-25 12:00:49 -0500

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -6,19 +6,33 @@ const app = express();
 const port = 5173;
 
 app.set("views", path.resolve(__dirname, "views"));
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
 // HOMEPAGE
-app.get('/', function(req,res){
-    const welcome = 'Welcome!'
-    const team = 'Team 7';
-    res.render('start', {
-        welcome: welcome,
-        team: team
-    });
-});
+app.get('/', function(req, res) {
+    res.render('start');
+  });
+  
+  app.get('/helpcenter', function(req, res) {
+    res.render('helpcenter');
+  });
+  
+  app.get('/settings', function(req, res) {
+    res.render('settings');
+  });
+  
+  app.get('/customerlookup', function(req, res) {
+    res.render('customerlookup');
+  });
+  
+  app.get('/createcustomer', function(req, res) {
+    res.render('createcustomer');
+  });
+  
+
 // start the express application on on port 8080
 app.listen(port, ()=>console.log('Application started listening on port '+port));
