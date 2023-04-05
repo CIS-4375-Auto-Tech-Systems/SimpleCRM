03e8f3a155efc5e7839025547daf886d724bd508
Nailbyte <stephanejlutz94@gmail.com>
2023-04-04 19:44:53 -0500

    some stuffs

--- frontend/app.js
+++ frontend/app.js
@@ -5,12 +5,6 @@ const axios =   require('axios');
 const app = express();
 const port = 5173;
 
-/////////////////////////////////////////////////////////////////////////
-//this is for the customer lookup page, still trying
-//const FromtheBACK = require('server.js');
-//app.use('/lookup', FromtheBACK);
-/////////////////////////////////////////////////////////////////////////
-
 app.set("views", path.resolve(__dirname, "views"));
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine","ejs");
@@ -38,10 +32,7 @@ app.get('/', function(req, res) {
   app.get('/createcustomer', function(req, res) {
     res.render('createcustomer');
   });
-//temporary nav link here I just wanted to see how it looks 
-  app.get('/customer', function(req, res) {
-    res.render('customer');
-  });
+  
 
 // start the express application on on port 8080
 app.listen(port, ()=>console.log('Application started listening on port '+port));
