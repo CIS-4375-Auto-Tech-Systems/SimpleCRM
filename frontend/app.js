1de97f2b89fa03f44b93906f80b01d1e6837219a
xlwillia <67807020+xlwillia@users.noreply.github.com>
2023-04-11 10:22:49 -0500

    Added a success page for popup success

--- frontend/app.js
+++ frontend/app.js
@@ -440,5 +440,9 @@ app.get('/', async function(req, res) {
 
   });
   
+  app.get('/success', function (req,res){
+    res.render('success');
+  });
+
 // start the express application on on port 8080
 app.listen(port, ()=>console.log('Application started listening on port '+port));
